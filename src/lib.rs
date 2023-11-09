19dc885c9adc8f191dd956c308e28e7749b1b52a
seba42 <fiasebastian.04@gmail.com>
2023-11-09 18:14:34 +0100

    added health check integration test with reqwest

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,28 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::dev::Server;
 
+pub const HTTP_PORT: u32 = 8000;
+pub const LOCAL_HOST: &'static str = "http://127.0.0.1";
+
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+pub fn build_server() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run();
+
+    Ok(server)
+}
---
+++ tests/health_check.rs
@@ -0,0 +1,24 @@
+use zero2prod::{HTTP_PORT, LOCAL_HOST};
+use reqwest;
+
+#[tokio::test] 
+async fn health_check_works() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+    let health_check_url = format!("{}:{}/health_check", LOCAL_HOST, HTTP_PORT);
+
+    let response = client
+        .get(health_check_url)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::build_server().expect("failed to build server");
+    let _ = tokio::spawn(server);
+}
