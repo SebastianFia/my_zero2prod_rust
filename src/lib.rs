12440df119da14e2c917fdd793032a145348f103
seba42 <fiasebastian.04@gmail.com>
2023-11-10 02:06:55 +0100

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 pub const DEFAULT_PORT: &str = "8000";
 pub const LOCAL_HOST: &str = "127.0.0.1";
 
-pub mod startup;
 pub mod routes;
+pub mod startup;
 
 pub use startup::build_server;
-
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,7 @@
-mod health_check;
 mod greet;
+mod health_check;
 mod subscriptions;
 
-pub use health_check::*;
 pub use greet::*;
+pub use health_check::*;
 pub use subscriptions::*;
